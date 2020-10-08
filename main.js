39b083c7f29753e257ca134221aff627b6a58c7c
adviislam10 <71040123+adviislam10@users.noreply.github.com>
2020-10-08 14:21:27 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 //Q1
 function removeLongNames(names) {
-    let array = names;
 
-    for (let i = 0; i < array.length; i++) {
-        if (array[i].length > 4) {
-            array.splice(i--, 1);
+    for (let i = 0; i < names.length; i++) {
+        if (names[i].length > 4) {
+            names.splice(i--, 1);
         }
     }
 }
 
 //Q2
 function removeAll(anArray, item) {
-    let array = anArray;
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] == item) {
-            array.splice(i--, 1);
+    for (let i = 0; i < anArray.length; i++) {
+        if (anArray[i] == item) {
+            anArray.splice(i--, 1);
         }
     }
 }
-
